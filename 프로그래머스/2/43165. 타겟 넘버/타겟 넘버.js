8cb5e89e764ce8f4e6c3077ef73dd422a833fb79
Sun <128468243+WonYunSun@users.noreply.github.com>
2025-09-25 14:44:52 +0900

    [level 2] Title: 타겟 넘버, Time: 13.70 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,23 @@
+function solution(numbers, target) {
+
+    let ans = 0;
+
+    function dfs(index, sum){
+        if(index === numbers.length){
+            if(sum === target) {
+                ans++;
+            }
+            return;
+        }
+
+        dfs(index +1, sum + numbers[index])
+        dfs(index +1, sum - numbers[index])
+
+    }
+    
+
+    dfs(0,0);
+
+    return ans
+
+}
