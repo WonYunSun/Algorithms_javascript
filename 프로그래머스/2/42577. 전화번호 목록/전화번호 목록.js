5f7fccf218b4b73d45af02f1003f9da311a2f07b
Sun <128468243+WonYunSun@users.noreply.github.com>
2025-09-27 15:26:36 +0900

    [level 2] Title: 전화번호 목록, Time: 138.77 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,13 +1,9 @@
 function solution(phone_book) {
-  const set = new Set(phone_book);
-  
-  for (const num of phone_book) {
-    for (let i = 1; i < num.length; i++) {
-      if (set.has(num.slice(0, i))) {
-        return false;
-      }
-    }
-  }
-  
-  return true;
+    //한 번호가 다른 번호의 접두어면 false
+    //전부탐색해야 하는 줄 알았는데, 이렇게 비슷한 단어를 찾는다? => 사전처럼 정렬
+    //some : 배열에서 하나만 true면 true반환
+    return !phone_book.sort().some((num, i)=>{//some을 역이용(?)
+        if(i === phone_book.length -1) return false;  //마지막까지 검사한거면 접두어 없음
+        return phone_book[i+1].startsWith(phone_book[i]);
+    })
 }
